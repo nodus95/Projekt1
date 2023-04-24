4e2aab780db4c24130ff6be109113f770f68cbb8
nodus95 <cawrownica@gmail.com>
2023-04-24 10:26:04 +0200

    wyjebalem sigmy

--- Bl_2_PL2000.py
+++ Bl_2_PL2000.py
@@ -1,12 +1,5 @@
 import numpy as np
 from math import *
-def sigma(f, a, e2):
-    A0 = 1 - e2/4 - 3 * e2**2/64 - 5 * e2**3/256
-    A2 = (3/8) * (e2 + e2**2/4 + 15*e2**3/128)
-    A4 = (15/256) * (e2**2 + (3 * e2**3)/4)
-    A6 = 35 * e2**3/3072
-    sigma = a * (A0*f - A2*sin(2*f) + A4*sin(4*f) - A6*sin(6*f))
-    return sigma
 def fl2pl2000(f,l,a,e2,ns,m0= 0.999923):
     if ns == 5:
         l0 = radians(15)
@@ -22,8 +15,12 @@ def fl2pl2000(f,l,a,e2,ns,m0= 0.999923):
     t = tan(f)
     n2 = ep2 * cos(f)**2
     N = a / np.sqrt(1 - e2 * sin(f)**2)
-    sigm = sigma(f,a,e2)
-    xgk = sigm + (dl**2/2) * N * sin(f)*cos(f)*(1 + (dl**2/12)*cos(f)**2*(5-t**2+9*n2+4*n2**2)+ ((dl**4)/360)*cos(f)**4*(61 - 58*t**2 + t**4 + 270*n2 - 330*n2*t**2))
+    A0 = 1 - e2/4 - 3 * e2**2/64 - 5 * e2**3/256
+    A2 = (3/8) * (e2 + e2**2/4 + 15*e2**3/128)
+    A4 = (15/256) * (e2**2 + (3 * e2**3)/4)
+    A6 = 35 * e2**3/3072
+    sigma = a * (A0*f - A2*sin(2*f) + A4*sin(4*f) - A6*sin(6*f))
+    xgk = sigma + (dl**2/2) * N * sin(f)*cos(f)*(1 + (dl**2/12)*cos(f)**2*(5-t**2+9*n2+4*n2**2)+ ((dl**4)/360)*cos(f)**4*(61 - 58*t**2 + t**4 + 270*n2 - 330*n2*t**2))
     ygk = dl*N*cos(f)*(1+(dl**2/6)*cos(f)**2*(1 - t**2 + n2) + (dl**4/120)*cos(f)**4*(5 - 18*t**2 + t**4 + 14*n2 - 58*n2*t**2))
     x2000 = xgk * m0
     y2000 = ygk * m0 + ns * 1000000 + 500000
--- XYZ_2_NEU.py
+++ XYZ_2_NEU.py
@@ -1,9 +1,7 @@
 from math import *
 import numpy as np
-def Rneu(f, l):
+def XYZ2neu(dX,f,l):
     R = np.array([[-sin(f) * cos(l), -sin(l), cos(f) * cos(l)],
                   [-sin(f) * sin(l), cos(l), cos(f) * sin(l)],
                   [cos(f), 0, sin(f)]])
-def XYZ2neu(dX,f,l):
-    R = Rneu(f,l)
     return(R.T @ dX)
