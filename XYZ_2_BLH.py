05cfe3fef583923e73bd6334645afe15b979a955
nodus95 <cawrownica@gmail.com>
2023-04-24 14:07:21 +0200

    dodalem wywolywanie danych w pliku wynikowym

--- XYZ_2_BLH.py
+++ XYZ_2_BLH.py
@@ -15,18 +15,35 @@ def xyz2flh(X, Y, Z, a, e2):
     
     l = np.arctan2(Y, X)
     return(f, l, h)
+def dms(x):
+    znak = ' '
+    if x < 0:
+        znak = '-'
+        x = abs(x)
+    x = x * 180/pi
+    d = int(x)
+    m = int((x - d) * 60)
+    s = (x - d - m/60) *3600
+    return(d, m, s)
 #Użycie:
-
-f = open('wspXYZ.txt')
-lines = f.readlines()[1:]
+f = open('wsp_inp.txt')
+lines = f.readlines()[4:]
 dane = []
 for line in lines:
     line = line.strip()
-    line = line.replace(',', '.')
-    wsp = line.split()
+    wsp = line.split(',')
     dane.append(wsp)
 macierzWSP = np.array(dane, dtype=float)
 f.close
+wynik= 'wynik.txt'
+f1= open(wynik, 'w')
+f1.write(f'fi                lambda        h\n')
 for i in macierzWSP:
     fi, lam, h = xyz2flh(i[0], i[1], i[2], a = 6378137.000, e2 = 0.00669438002290)
     print(fi, lam, h)
+    fid, fim, fis = dms(fi)
+    lamd, lamm, lams = dms(lam)
+    fis = round(fis, 5)
+    lams = round(lams, 5)
+    h = round(h, 3)
+    f1.write(f'{fid}°{fim} {fis}, {lamd}°{lamm} {lams}, {h} m\n')
